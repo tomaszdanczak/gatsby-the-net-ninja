6d27640b9aed212e2e63eba8f1ec6f8cf74f06f6
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-04 18:59:56 +0200

    feat: render <Link /> components using data from markdown files

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,14 +1,43 @@
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
 
-export default function Projects() {
+export default function Projects({ data }) {
+  const projects = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div>
+          {projects.map(project => (
+            <Link to={'/projects/' + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+// exports page query
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          stack
+          title
+          slug
+        }
+        id
+      }
+    }
+  }
+`
