4e42b4752af4791126f904b369667c98b7f03a3e
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-04 17:40:04 +0200

    feat: install gatsby-source-file plugin and configure it to look at markdown file in src/projects/

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+  ],
   siteMetadata: {
     title: 'Web Warrior',
     description: 'web dev portfolio',
