9809c565a22b4ce7e50b33e788e8d25dc763329a
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-04 13:44:41 +0200

    feat: create Layout component and use it in page components

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <Navbar />
+      <div className="content">{children}</div>
+      <Footer />
+    </div>
+  )
+}
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function About() {
   return (
-    <div>
-      <h1>About page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae
-        praesentium, est fuga laudantium molestias perferendis cupiditate natus
-        rem blanditiis exercitationem deleniti itaque at in modi illo? Minus,
-        quisquam nisi?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae
-        praesentium, est fuga laudantium molestias perferendis cupiditate natus
-        rem blanditiis exercitationem deleniti itaque at in modi illo? Minus,
-        quisquam nisi?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae
-        praesentium, est fuga laudantium molestias perferendis cupiditate natus
-        rem blanditiis exercitationem deleniti itaque at in modi illo? Minus,
-        quisquam nisi?
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae
+          praesentium, est fuga laudantium molestias perferendis cupiditate
+          natus rem blanditiis exercitationem deleniti itaque at in modi illo?
+          Minus, quisquam nisi?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae
+          praesentium, est fuga laudantium molestias perferendis cupiditate
+          natus rem blanditiis exercitationem deleniti itaque at in modi illo?
+          Minus, quisquam nisi?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae
+          praesentium, est fuga laudantium molestias perferendis cupiditate
+          natus rem blanditiis exercitationem deleniti itaque at in modi illo?
+          Minus, quisquam nisi?
+        </p>
+      </div>
+    </Layout>
   )
 }
