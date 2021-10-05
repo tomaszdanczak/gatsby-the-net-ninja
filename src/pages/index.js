cbf3048bbc759544d8af40577cdd9867044d13db
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-05 09:37:09 +0200

    feat: add image using <Img /> from gatsby and GraphQL layer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../components/Layout'
 import * as styles from '../styles/home.module.css'
+import Img from 'gatsby-image'
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout>
       <section className={styles.header}>
@@ -15,8 +16,20 @@ export default function Home() {
             My Portfolio Projects
           </Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }} />
+        <Img fluid={data.file.childImageSharp.fluid} />
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query Banner {
+    file(relativePath: { eq: "banner.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
